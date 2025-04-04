844ce46b88c1cb9351e89f1bf3c4e0e29cb0cc4e
m4tiss <mateusz039@op.pl>
2025-04-04 17:53:57 +0200

    add settings Tile

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import ProfileCard from "./ProfileCard";
 import SidebarTile from "./SidebarTile";
+import { Settings } from "lucide-react";
 import { sidebarItems } from "../data/SidebarItems";
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col items-center w-1/6 h-screen py-10 px-8">
+    <div className="flex flex-col items-center w-1/6 h-screen justify-between py-10 px-8">
       <div className="flex flex-col items-center gap-10 w-full">
         <ProfileCard />
         <ul className="w-full">
@@ -20,6 +21,11 @@ const Sidebar = () => {
             </li>
           ))}
         </ul>
+      </div>
+      <div className="w-full">
+        <Link to="/settings" className="w-full">
+          <SidebarTile icon={<Settings size={30} />} title={"Settings"} />
+        </Link>
       </div>
     </div>
   );
